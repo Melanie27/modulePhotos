9426444de90c4b04ba066d6fb62443d22fe5b616
Melanie27 <melaniemcganney@gmail.com>
2017-02-10 11:20:28 -0800

    calling datasource causing errors
    do we need the datasource for this exercise?

--- modulePhotos/modulePhotos/AddPetProfileViewController.m
+++ modulePhotos/modulePhotos/AddPetProfileViewController.m
@@ -9,10 +9,12 @@
 #import "AddPetProfileViewController.h"
 
 #import "PCDataSource.h"
-//#import <Photos/Photos.h>
-//#import "Pet.h"
+#import <Photos/Photos.h>
+#import "Pet.h"
 
-@interface AddPetProfileViewController ()
+@interface AddPetProfileViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate>
+
+@property (nonatomic, strong) NSString *downloadURLString;
 
 @end
 
@@ -20,7 +22,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.title = @"Add Pet";
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardFrameWillChange:) name:UIKeyboardWillChangeFrameNotification object:nil];
     // Do any additional setup after loading the view.
+    
+}
+
+-(void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
+}
+
+- (void)keyboardFrameWillChange:(NSNotification *)notification
+{
+    CGRect keyboardEndFrame = [[[notification userInfo] objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    CGRect keyboardBeginFrame = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue];
+    UIViewAnimationCurve animationCurve = [[[notification userInfo] objectForKey:UIKeyboardAnimationCurveUserInfoKey] integerValue];
+    NSTimeInterval animationDuration = [[[notification userInfo] objectForKey:UIKeyboardAnimationDurationUserInfoKey] integerValue];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    [UIView setAnimationCurve:animationCurve];
+    
+    CGRect newFrame = self.view.frame;
+    CGRect keyboardFrameEnd = [self.view convertRect:keyboardEndFrame toView:nil];
+    CGRect keyboardFrameBegin = [self.view convertRect:keyboardBeginFrame toView:nil];
+    
+    newFrame.origin.y -= (keyboardFrameBegin.origin.y - keyboardFrameEnd.origin.y);
+    self.view.frame = newFrame;
+    
+    [UIView commitAnimations];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -37,5 +72,32 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark - UIImagePicker Delegate Methods
+
+- (IBAction)uploadProfilePhoto:(id)sender {
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeSavedPhotosAlbum]) {
+        UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+        picker.delegate = self;
+        picker.allowsEditing = YES;
+        picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+        
+        [self presentViewController:picker animated:YES completion:NULL];
+        
+    }
+    
+}
+
+-(void)sendPetInfoToFirebase {
+    //PCDataSource *pc = [PCDataSource sharedInstance];
+}
+
+- (IBAction)savePetProfile:(id)sender {
+    
+    [self sendPetInfoToFirebase];
+  
+}
+
+
 
 @end
